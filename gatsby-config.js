d41587886c04d9f1ee95ff4ee6e98ad261f611f3
camel113 <adrienbigler@gmail.com>
2018-03-26 16:03:14 +0200

    Remove GA plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,6 @@ module.exports = {
   pathPrefix: `/ecg-landing`,
   plugins: [
 	  {
-	    resolve: `gatsby-plugin-google-analytics`,
-	    options: {
-	      trackingId: "UA-36728721-1",
-	      // Puts tracking script in the head instead of the body
-	      head: false
-	    }
-	  },
-	  {
 	  	resolve:`gatsby-plugin-react-helmet`
 	  },
 	  {
